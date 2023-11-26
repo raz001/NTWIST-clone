9817a0f3b3f3321168a9ea26c704b60f8ec8952a
raz001 <razbhagat1@gmail.com>
2023-11-27 01:06:38 +0530

    navbar and hero part added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero';
+
+const Home = () => {
+  return (
+    <>
+    <Hero/>
+    {/* <Content/>
+    <Footer/> */}
+    </>
+  )
+}
+
+export default Home
